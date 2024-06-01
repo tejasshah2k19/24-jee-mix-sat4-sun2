6ca6d30d789a597478fb42537e235c789c381ccf
Tejas Shah <tejasshah2k19@gmail.com>
2024-06-01 17:01:39 +0530

    servlet life cycle

---
+++ src/main/java/com/google/controller/ServletLifeCycle.java
@@ -0,0 +1,29 @@
+package com.google.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ServletLifeCycle")
+public class ServletLifeCycle extends HttpServlet {
+
+	public void init() throws ServletException {
+		//connection 
+		System.out.println("ServletLifeCycle::init()");
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("ServletLifeCycle::service()");
+	}
+
+	public void destroy() {
+		//close 
+		System.out.println("ServletLifeCycle::destroy()");
+	}
+	
+}
