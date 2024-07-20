c36fd43e3dfcb36c3e3c740df1bd00e8db2d074a
Tejas Shah <tejasshah2k19@gmail.com>
2024-07-20 16:47:34 +0530

    session 1

---
+++ src/main/java/com/google/controller/UserNameSessionServlet.java
@@ -0,0 +1,28 @@
+package com.google.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/UserNameSessionServlet")
+public class UserNameSessionServlet extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String userName = request.getParameter("userName");
+
+		// server -> get
+		HttpSession session = request.getSession();// get the existing session or create new if not found
+		// getSession(false);
+
+		session.setAttribute("userName", userName);
+		
+		response.sendRedirect("SessionHome.jsp");
+	}
+}
